83410a5f9fd5f2db9382795db4eb9331e0d63838
Berk Geveci <berk.geveci@kitware.com>
2001-10-26 12:58:01 -0400

    Regression testing.

---
+++ Utility/rtImage.tcl
@@ -0,0 +1,36 @@
+proc pvImageTest { ValidImageName threshold } {
+    
+    vtkWindowToImageFilter _temp_w2if
+    _temp_w2if SetInput RenWin1
+
+    if {[file exists $ValidImageName] == 0 } {
+	if {[catch {set channel [open $ValidImageName w]}] == 0 } {
+	    close $channel
+	    vtkPNGWriter _temp_pngw
+	    _temp_pngw SetFileName $ValidImageName
+	    _temp_pngw SetInput [_temp_w2if GetOutput]
+	    _temp_pngw Write
+	} else {
+	    Application Exit
+	}
+    }
+
+    vtkPNGReader _temp_png
+    _temp_png SetFileName $ValidImageName
+    
+    vtkImageDifference _temp_id
+    _temp_id SetInput [_temp_w2if GetOutput]
+    _temp_id SetImage [_temp_png GetOutput]
+    _temp_id Update
+    set imageError [_temp_id GetThresholdedError]
+    set minError [_temp_id GetThresholdedError]
+    
+    _temp_w2if Delete 
+    _temp_id Delete
+    _temp_png Delete
+
+    if { $minError > $threshold } {
+	Application SetExitStatus 1
+    } 
+
+}
